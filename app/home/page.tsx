477c7470ece66231dfafa2e64344a4855bcfab64
Kim dong tak <59105154+Dong-tak@users.noreply.github.com>
2024-11-25 00:25:36 +0900

    중앙 위젯 구현

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,17 +2,35 @@
 
 import HomeView from "@/components/home/homeview";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { addWidget } from "@/lib/redux/features/whiteboardSlice";
+import { createBoardWithTitle } from "./action";
 
 export default function Home() {
   const [contentTitle, setContentTitle] = useState("");
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const dispatch = useDispatch();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContentTitle(e.target.value);
   };
 
   const handleSaveClick = () => {
-    setOpen(true);
+    if (contentTitle.trim()) {
+      const boardPosition = {
+        x: window.innerWidth / 2 - 250,
+        y: window.innerHeight / 2 - 250,
+      };
+
+      const newBoard = createBoardWithTitle(contentTitle, [], boardPosition);
+
+      dispatch(addWidget(newBoard));
+
+      localStorage.setItem("currentBoardId", "1");
+      router.push("1/1");
+    }
   };
 
   const onOpenChange = (open: boolean) => {
