31d868ab5b8fec7d44052771e169519d428e38ea
Kim dong tak <59105154+Dong-tak@users.noreply.github.com>
2024-11-27 11:03:36 +0900

    test

--- components/widget/widgetShell.tsx
+++ components/widget/widgetShell.tsx
@@ -377,7 +377,7 @@ export default function WidgetShell({
   };
 
   const handleTextChange = (text: string) => {
-    if (widget.innerWidget.type === 'text') {
+    if (widget.innerWidget.type === "text") {
       dispatch(
         updateWidget({
           ...widget,
@@ -782,7 +782,7 @@ export default function WidgetShell({
   };
 
   const handleWidgetClick = (e: React.MouseEvent) => {
-    console.log('widgetFrom:', widget.from, 'widgetTo:', widget.to);
+    console.log("widgetFrom:", widget.from, "widgetTo:", widget.to);
     if (isArrowMode) {
       dispatch(addLinkWidgets(widget));
     }
@@ -945,9 +945,9 @@ export default function WidgetShell({
               ? "2px solid #FFB300" //amber-500
               : "none"
           }`,
-          outlineOffset: '0px', // 음수 값을 주면 안쪽으로 들어갑니다
-          borderRadius: '4px',
-          transition: 'height 0.3s ease-in-out', // 높이 ���경 애니메이션 추가
+          outlineOffset: "0px", // 음수 값을 주면 안쪽으로 들어갑니다
+          borderRadius: "4px",
+          transition: "height 0.3s ease-in-out", // 높이 ���경 애니메이션 추가
         }}
         onClick={handleWidgetClick}
         onMouseDown={handleMouseDown}
