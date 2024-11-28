c616d2b9c0b9d3027158d54e612bb65c253774d0
ToTheGit <nws717@naver.com>
2024-11-28 23:31:33 +0900

    Fix undo and redo functionality in whiteboard.tsx

--- components/board/whiteboard.tsx
+++ components/board/whiteboard.tsx
@@ -27,8 +27,8 @@ import {
   addWidgetTo,
   updateWidget,
   setInitialWidgets,
-  // redo,
-  // undo,
+  redo,
+  undo,
 } from "@/lib/redux/features/whiteboardSlice";
 
 import {
@@ -1027,11 +1027,11 @@ export default function Whiteboard() {
         if (e.shiftKey && e.key.toLowerCase() === "z") {
           // Cmd/Ctrl + Shift + Z: Redo
           e.preventDefault();
-          // dispatch(redo());
+          dispatch(redo());
         } else if (e.key.toLowerCase() === "z") {
           // Cmd/Ctrl + Z: Undo
           e.preventDefault();
-          // dispatch(undo());
+          dispatch(undo());
         }
       }
     };
